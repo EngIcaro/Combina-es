a41fc1b4e9f57d4bd7fcb03f1709a34242178713
Your Name <icarogabriel7@gmail.com>
2020-10-08 07:19:36 -0300

    add dictionary

--- fragmentando.py
+++ fragmentando.py
@@ -2,8 +2,7 @@
 # Autor: Ícaro Gabriel Paiva Bastos
 # Última modificação: 07/10/2020
 
-entrada = "icaro meu querido icaro rato oi"
-
+#%% Definição da função splitter
 def splitter(str):
     for i in range(1, len(str)):
         start = str[0:i]
@@ -14,3 +13,17 @@ def splitter(str):
             result.extend(split)
             yield result
 
+#%% Lendo a frase de entrada
+entrada = input("Olá, seja bem-vindo!\nO nosso sistema é capaz de gerar todas as combinações possíveis de quebra de uma frase\nDigite uma frase:\n")
+frase = entrada
+
+#%% Mapeando cada palavra em um número correspondente
+dicionario = {}
+aux = 0
+for i in frase.split():
+    dicionario[str(aux)] = i
+    frase = frase.replace(i, str(aux))
+    aux += 1
+    
+frase= frase.replace(' ', '')
+#%%
